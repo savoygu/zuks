12825d408539d79ceac7891ab9618d99d83809dc
gusaifei <gusaifei@zongheng.com>
2023-07-06 10:57:02 +0800

    Plan document structure

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -1,28 +1,56 @@
 import { defineConfig } from 'vitepress'
 
+const Guide = [
+  { text: '开始吧', link: '/guide/' },
+  { text: '最佳实践', link: '/guide/best-practice' },
+]
+
+const CoreFunctions = [
+  { text: '远程搜索', link: '/core/useRemoteSearch/' },
+]
+
+const DefaultSidebar = [
+  { text: '指南', items: Guide },
+  { text: '核心功能', items: CoreFunctions },
+]
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: 'zuks',
-  description: 'zuks',
+  title: 'Zuks',
+  description: 'Vue 组件库组合工具集',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
-    ],
-
-    sidebar: [
       {
-        text: 'Examples',
+        text: '指南',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: '指南', items: Guide },
         ],
       },
+      // { text: 'Examples', link: '/markdown-examples' },
     ],
 
+    sidebar: {
+      '/guide/': DefaultSidebar,
+      '/core/': DefaultSidebar,
+    },
+
+    outline: {
+      label: '在这个页面上',
+    },
+
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'github', link: 'https://github.com/savoygu/zuks' },
     ],
+
+    editLink: {
+      pattern: 'https://github.com/savoygu/zuks/tree/main/src/:path',
+      text: '在GitHub上编辑此页面',
+    },
+
+    docFooter: {
+      prev: '上一页',
+      next: '下一页',
+    },
   },
 })
--- src/index.md
+++ src/index.md
@@ -3,23 +3,26 @@
 layout: home
 
 hero:
-  name: "zuks"
-  text: "zuks"
-  tagline: My great project tagline
+  name: "Zuks"
+  text: "Vue 组件库组合工具集"
+  tagline: 基本的 Vue 组件库组合工具集
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
+      text: 开始吧
+      link: /guide/
     - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 在Github上查看
+      link: https://github.com/savoygu/zuks
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 功能丰富
+    details: n 多个函数供您选择
+    icon: 🎛
+  - title: 无缝迁移
+    details: 同时适用于 Vue 3 和 2
+    icon: 🚀
+  - title: 完全可树摇
+    details: 只取你想要的东西
+    icon: ⚡
 ---
 
